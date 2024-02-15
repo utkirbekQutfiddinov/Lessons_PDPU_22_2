fa64e0aee3b168d844e10eedfb80c65c33ba4a61
utkirbek <utkirbek@mail.ru>
2024-02-15 19:56:07 +0500

    git advanced

---
+++ src/lessons/module_4/lesson_4_9/GitAdvanced.java
@@ -0,0 +1,21 @@
+package lessons.module_4.lesson_4_9;
+
+public class GitAdvanced {
+    public static void main(String[] args) {
+        /** Git basic commands:
+         * 1) git init, git clone url
+         * 2) git branch branch-name
+         * 3) git branch -d branchName
+         * 4) git checkout branchName
+         * 5) git status
+         * 6) git add fileName, git add .
+         * 7) git commit -m "text"
+         * 8) git pull
+         * 9) git push
+         * 10) git merge
+         *
+         */
+
+
+    }
+}
